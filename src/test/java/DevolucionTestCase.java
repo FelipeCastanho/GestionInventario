4088b869d05bcf440c036ad70878992790ea9ea2
Felipe Castaño <castano.felipe.95@gmail.com>
2019-04-02 14:06:37 -0500

    Agregando test suite

--- src/test/java/DevolucionTestCase.java
+++ src/test/java/DevolucionTestCase.java
@@ -1,23 +1,23 @@
 package test.java;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
-class DevolucionTestCase {
+public class DevolucionTestCase {
 
-	@BeforeEach
-	void setUp() throws Exception {
+	@Before
+	public void setUp() throws Exception {
 	}
 
-	@AfterEach
-	void tearDown() throws Exception {
+	@After
+	public void tearDown() throws Exception {
 	}
 
 	@Test
-	void test() {
+	public void test() {
 		fail("Not yet implemented");
 	}
 
