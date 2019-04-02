b6774c3bf0cd0ef01f228a51d5d5b2318692c270
Felipe Castaño <castano.felipe.95@gmail.com>
2019-04-02 13:59:18 -0500

    Cambiando a JUnit jupiter test

--- target/test-classes/DevolucionTestCase.java
+++ target/test-classes/DevolucionTestCase.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class DevolucionTest {
+class DevolucionTestCase {
 
 	@BeforeEach
 	void setUp() throws Exception {
