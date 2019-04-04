21048d72cc03d676490f2cd353ed19a9278a5f7d
konaizumi <maria.aguiar@correounivalle.edu.co>
2019-04-04 10:35:34 -0500

    Fixing testcase error

--- src/test/java/ProductoTestCase.java
+++ src/test/java/ProductoTestCase.java
@@ -73,6 +73,6 @@ public class ProductoTestCase {
 	@Test
 	public void testListarProductos() {
 		List productos = productoLogica.listarProductos();
-		assertEquals(5, productos.size());
+		assertEquals(11, productos.size());
 	}
 }
